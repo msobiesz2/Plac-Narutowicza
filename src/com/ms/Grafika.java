eb800a149dd53b2cfff433ff697f046c4bcc15c1
Marek <msobiesz2@gmail.com>
2019-07-23 15:57:49 +0200

    again new paths

--- src/com/ms/Grafika.java
+++ src/com/ms/Grafika.java
@@ -49,9 +49,12 @@ public class Grafika extends Application{
 		//stackPane.getChildren().add(button);
 		
 		final Circle car = new Circle(3,3,3);
-		car.setCenterX(-356.0f);
-		car.setCenterY(77.0f);
-		car.setFill(Color.DARKRED);
+		//car.setCenterX(-356.0f); to first 
+		//car.setCenterY(77.0f);	and second path
+		car.setCenterX(-110.0f); 	//third and
+		car.setCenterY(380.0f); 	// forth path
+		
+		car.setFill(Color.BLACK);
 		stackPane.getChildren().add(car);
 		
 		//Text text = new Text("This is a car, darkred car");
@@ -60,21 +63,39 @@ public class Grafika extends Application{
 		
 		Path path = new Path();
 		//path.getElements().add(new MoveTo(0,0));
-		//path.getElements().add(new CubicCurveTo(-160,-20,-170,-25,-175,-36));		//first path
+		//path.getElements().add(new CubicCurveTo(-160,-20,-170,-25,-175,-36));		//first path moveto 00
 		//path.getElements().add(new CubicCurveTo(-190,-50,-200,-90,-242,-175));
 		//path.getElements().add(new CubicCurveTo(-243,-173,-242,-178,-239,-205));
 		//path.getElements().add(new CubicCurveTo(-235,-220,-218,-250,-165,-350));
 		
 		
-		path.getElements().add(new MoveTo(0,0));
-		path.getElements().add(new CubicCurveTo(-160,-20,-170,-25,-175,-36));
-		path.getElements().add(new CubicCurveTo(-190,-50,-200,-90,-242,-133));
-		path.getElements().add(new CubicCurveTo(-190,-50,-200,-90,-242,-133));
+		path.getElements().add(new MoveTo());
+		//path.getElements().add(new CubicCurveTo(-160,-20,-170,-25,-190,-36));  second path move to 00
+		//path.getElements().add(new CubicCurveTo(-195,-50,-210,-90,-250,-133)); 
+		//path.getElements().add(new CubicCurveTo(-255,-140,-280,-150,-330,-130)); 
+		//path.getElements().add(new CubicCurveTo(-333,-130,-400,-10,-725,600));
+		
+		//path.getElements().add(new CubicCurveTo(-100,200,-150,220,-310,590)); //third path
+		//path.getElements().add(new CubicCurveTo(-305,595,-300,620,-270,650));
+		//path.getElements().add(new CubicCurveTo(-260,660,-235,650,-200,655));
+		//path.getElements().add(new CubicCurveTo(-195,642,-170,639,-130,645));
+		//path.getElements().add(new CubicCurveTo(-130,660,-70,630,-12,580));
+		//path.getElements().add(new LineTo(160,310));
+		//path.getElements().add(new CubicCurveTo(165,300,179,300,265,305));
+		
+		
+		path.getElements().add(new CubicCurveTo(-100,200,-150,220,-310,590)); //forth path
+		path.getElements().add(new CubicCurveTo(-305,595,-300,620,-270,650));
+		path.getElements().add(new CubicCurveTo(-260,660,-235,650,-200,655));
+		path.getElements().add(new CubicCurveTo(-195,642,-170,639,-130,645));
+		path.getElements().add(new CubicCurveTo(-130,660,-70,630,-12,580));
+		path.getElements().add(new LineTo(80,400));
+		path.getElements().add(new CubicCurveTo(70,300,60,220,30,200));
 		
 		PathTransition pathT = new PathTransition();
-		pathT.setDuration(Duration.millis(5000));
+		pathT.setDuration(Duration.millis(10000));
 		pathT.setPath(path);
-		pathT.setPath(path2);
+		//pathT.setPath(path2);
 		pathT.setNode(car);
 		//pathT.setCycleCount(Timeline.INDEFINITE);
 		pathT.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
